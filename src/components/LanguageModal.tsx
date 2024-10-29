887f18eef636858bbbbecbae55ac85191b64a3f2
Schita01 <129902293+Schita01@users.noreply.github.com>
2024-10-29 18:57:53 +0400

    Add files via upload

---
+++ src/components/LanguageModal.tsx
@@ -0,0 +1,31 @@
+const LanguageModal = ({
+  setLanguageSelected,
+}: {
+  setLanguageSelected: (value: string) => void;
+}) => {
+  const handleLanguageSelect = (lang: string) => {
+    console.log(`Selected Language: ${lang}`);
+    setLanguageSelected(lang); // ენების არჩევის შემდეგ ცვლილების ნიშანი
+  };
+
+  return (
+    <div className="modal">
+      <h1>FT</h1>
+      <select
+        className="language-select"
+        onChange={(e) => handleLanguageSelect(e.target.value)}
+        defaultValue=""
+      >
+        <option value="" disabled>
+          Select a language
+        </option>
+
+        <option value="geo">ქართული</option>
+        <option value="eng">English</option>
+        <option value="rus">Русский</option>
+      </select>
+    </div>
+  );
+};
+
+export default LanguageModal;
